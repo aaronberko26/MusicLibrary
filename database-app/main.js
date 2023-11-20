edf9ea9d10ba3aa7bd2ee28a7d8180914139f79a
Wei Lee <73676019+weilee90@users.noreply.github.com>
2023-11-19 19:10:03 -0700

    Initial

---
+++ database-app/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadURL("http://localhost:3000"); // Assuming your React app runs on port 3000
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
